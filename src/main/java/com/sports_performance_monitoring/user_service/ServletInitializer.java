efbb140c9bd4de1197f5222b2bcd107bb2ea9e4f
balaji379 <vigneshbalaji.kvl@gmail.com>
2024-10-17 17:19:26 +0530

    user service project structure has created

---
+++ src/main/java/com/sports_performance_monitoring/user_service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sports_performance_monitoring.user_service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserServiceApplication.class);
+	}
+
+}
---
+++ src/test/java/com/sports_performance_monitoring/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports_performance_monitoring.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
